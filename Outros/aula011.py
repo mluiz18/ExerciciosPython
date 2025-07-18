20ad31ecada68a9e8340a66e3c20ca19495ef47a
mluiz18 <migueluiz18@gmail.com>
2025-07-18 19:58:15 -0300

    docs: "Aula de modularização e pacotes"

---
+++ Outros/aula011.py
@@ -0,0 +1,12 @@
+#Modularização
+#A Fim de manter minha sanidade mental, eu criei um novo projeto e testei a modularização lá
+#Básicamente o que eu fiz foi: criar uma função em um arquivo e importar ela para outro com o import
+#vale ressaltar que o recomendado é importar o modulo inteiro, mas caso os nomes não conflitem eu posso usar "from x import y"
+
+#Pacotes = Bibliotecas que você baixa (tipo: pandas, numpy, matplot)
+#quando você "instala" essas libs na verdade você está baixando um arquivo com todas essas funções ja feitas, ai e so importar
+
+#outra coisa:
+#arquivo (em python) = potencial modulo
+#pasta (em python) = potencial pacotes
+#ou seja, as pastas "Outros" ou "Faculdade", para o python são potenciais pacotes
