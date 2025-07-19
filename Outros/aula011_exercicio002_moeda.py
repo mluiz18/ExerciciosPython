ab9efcf5b8ebacaf3c25d3d66b86be0d7b6705c6
mluiz18 <migueluiz18@gmail.com>
2025-07-19 18:42:33 -0300

    docs: "Exercicio de Modularização"

---
+++ Outros/aula011_exercicio002_moeda.py
@@ -0,0 +1,14 @@
+import aula011
+
+def resumo(p, a, r):
+    aumento = aula011.aumentar(p, a)
+    reducao = aula011.diminuir(p, r)
+    metade = aula011.metade(p)
+    dobro = aula011.dobro(p)
+
+    print(f"-="*4, "RESUMO", "=-"*4)
+    print(f"PREÇO: {aula011.moeda(p)}")
+    print(f"METADE: {aula011.moeda(metade)}")
+    print(f"DOBRO: {aula011.moeda(dobro)}")
+    print(f"AUMENTO DE {a}%: {aula011.moeda(aumento)}")
+    print(f"REDUÇÃO DE {r}%: {aula011.moeda(reducao)}")
