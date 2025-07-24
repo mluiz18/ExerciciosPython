aafae19be31e5ceb4c565157c392c84def8433d4
mluiz18 <migueluiz18@gmail.com>
2025-07-24 20:04:08 -0300

    docs: "Desafios de lógica de programação"

--- teste.py
+++ teste.py
@@ -1,4 +1,21 @@
-for i in range(10):
-    a = 5
+# Escreva uma função que conte quantas vogais e consoantes existem em uma string.
 
-print(a)
+def contador(string):
+    c = 0
+    v = 0
+    text = string.upper().split()
+    for i in range(len(text)):
+        v_napalvra = 0
+        c_napalavra = 0
+        for j in range(len(text[i])):
+            if text[i][j].isalpha():
+                if text[i][j] in "AEIOUÁÀÉÈÍÌÚÙÓÒÂÊÎÔÛ":
+                    v_napalvra += 1
+                else:
+                    c_napalavra += 1
+        c += c_napalavra
+        v += v_napalvra
+    return c, v
+
+
+print(contador("Eu tenho 3 peixes"))
