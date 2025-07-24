8d6e67969d703f0675c8ee222fcab78fe2da371b
mluiz18 <migueluiz18@gmail.com>
2025-07-24 20:25:10 -0300

    docs: "Desafios de lógica de programação"

--- teste.py
+++ teste.py
@@ -1,21 +1,8 @@
-# Escreva uma função que conte quantas vogais e consoantes existem em uma string.
+# Receba uma frase e inverta a ordem das palavras, não os caracteres.
 
-def contador(string):
-    c = 0
-    v = 0
-    text = string.upper().split()
-    for i in range(len(text)):
-        v_napalvra = 0
-        c_napalavra = 0
-        for j in range(len(text[i])):
-            if text[i][j].isalpha():
-                if text[i][j] in "AEIOUÁÀÉÈÍÌÚÙÓÒÂÊÎÔÛ":
-                    v_napalvra += 1
-                else:
-                    c_napalavra += 1
-        c += c_napalavra
-        v += v_napalvra
-    return c, v
+def inversor_de_frase(frase):
+    texto = frase.split()
+    invertido = texto[::-1]
+    return " ".join(invertido)
 
-
-print(contador("Eu tenho 3 peixes"))
+print(inversor_de_frase("Eu amo pudim!"))
