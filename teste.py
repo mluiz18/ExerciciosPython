0d622bed4d8227888d1af5faa52653a223529073
mluiz18 <migueluiz18@gmail.com>
2025-07-24 22:35:14 -0300

    docs: "Desafios de lógica de programação"

--- teste.py
+++ teste.py
@@ -1,11 +1,6 @@
 n = int(input("Digite um número: "))
-s = 0
+f = 1
+for i in range(n):
+    f *= i+1
 
-if n < 1:
-    for i in range(n,1):
-        s += i
-else:
-    for i in range(1,n+1):
-        s += i
-
-print(f"A Soma de todos os número de 1 até {n} é {s}")
+print(f"{n}! = {f}")
