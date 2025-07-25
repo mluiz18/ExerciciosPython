b4c3c4a0bd46cdc313c3e08e9b21b5218a9dda27
mluiz18 <migueluiz18@gmail.com>
2025-07-24 22:50:01 -0300

    docs: "Desafios de lógica de programação"

--- teste.py
+++ teste.py
@@ -1,6 +1,11 @@
 n = int(input("Digite um número: "))
-f = 1
-for i in range(n):
-    f *= i+1
+c = 0
 
-print(f"{n}! = {f}")
+for i in range(1, n+1):
+    if n % i == 0:
+        c+=1
+
+if c == 2:
+    print(f"O Número {n} É primo!")
+else:
+    print(f"O Número {n} NÃO É primo!")
