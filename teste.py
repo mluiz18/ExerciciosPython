067289862b03b7c6b44e540d297d59dcad876be3
mluiz18 <migueluiz18@gmail.com>
2025-07-25 17:27:11 -0300

    docs: "Desafios de lógica de programação"

--- teste.py
+++ teste.py
@@ -1,18 +1,6 @@
-palavra = "arara"
-
-c = 0
-tamanho = len(palavra)
-e_palindromo = 0
-
-for i in range(tamanho):
-    if palavra[i] == palavra[tamanho-1]:
-        e_palindromo += 1
-    c += 1
-    tamanho -= 1
-    if e_palindromo > tamanho:
-        print(f"A Palavra {palavra} é um palíndromo!")
-        break
-    else:
-        if c == tamanho:
-            print(f"A Palavra {palavra} NÃO é um palíndromo!")
-            break
+palavra = "Renner"
+palavra_invertida = palavra[::-1]
+if palavra.upper() == palavra_invertida.upper():
+    print("É palíndromo!")
+else:
+    print("Não é palíndromo!")
